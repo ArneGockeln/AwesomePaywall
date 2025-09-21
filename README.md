65617d336280f667c5318e396785d5f0784b7881
ArneGockeln <arne@arnesoftware.com>
2025-09-21 11:12:11 +0200

    migrated paywall from active project.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AwesomePaywall
-A paywall and controller for macOS and iOS apps.
+A SwiftUI paywall and controller for macOS and iOS apps.
+
+## Requirements
+- iOS v16.4 is the minimum requirement.
+- A SwiftUI Project
+
+## 📃 License
+`AwesomePaywall` is available under the MIT license. See the [LICENSE](https://github.com/ArneGockeln/AwesomePaywall/blob/main/LICENSE) file for more info.
+
+## 📦 Projects
+
+The following projects have integrated AwesomePaywall in their App.
+
+- [PushUp Battle](https://arnesoftware.com/apps) available on the [AppStore]()
+- [Elated | Widget Counter](https://arnesoftware.com/apps) available on the [AppStore](https://apps.apple.com/de/app/elated-urlaubs-countdown-timer/id6740820297)
--- Sources/AwesomePaywall/AwesomePaywall.swift
+++ Sources/AwesomePaywall/AwesomePaywall.swift
@@ -1,2 +1,29 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct AwesomePaywallViewModifier: ViewModifier {
+    let backgroundColor: Color
+    let highlightColor: Color
+
+    @State private var isPaywallPresented: Bool = false
+    @StateObject private var storeManager: StoreManager = .shared
+
+    func body(content: Content) -> some View {
+        content
+            .fullScreenCover(isPresented: $isPaywallPresented) {
+                PaywallView(backgroundColor: backgroundColor, highlightColor: highlightColor, isPresented: $isPaywallPresented)
+            }
+            .environmentObject(storeManager)
+            .task {
+                await storeManager.configure()
+            }
+    }
+}
+
+extension View {
+    func awesomePaywall(backgroundColor: Color = Color.orange, highlightColor: Color = Color.red) -> some View {
+        modifier(AwesomePaywallViewModifier(backgroundColor: backgroundColor, highlightColor: highlightColor))
+    }
+}
